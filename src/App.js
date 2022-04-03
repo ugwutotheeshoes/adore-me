496b278df47a749a523a3a4b3dec497eca8f4a13
Emmanuel Ugwu <>
2022-04-03 21:44:24 +0100

    Changes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
-import React from "react";
 import ReactDOM from "react-dom";
-import Pet from './Pet'
+import Pet from "./Pet";
 
-// const Pet = (props) => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h2", {}, props.name),
-//     React.createElement("h3", {}, props.animal),
-//     React.createElement("h3", {}, props.breed),
-//   ]);
-// };
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
-    React.createElement(Pet, { name: "Luna", animal: "Cat", breed: "Tomcat" }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Parrot",
-    }),
-    React.createElement(Pet, {
-      name: "Bill",
-      animal: "Dog",
-      breed: "German Sheperd",
-    })
+  return (
+    <div>
+      <h1>Adore Me!</h1>
+      <Pet name="Luna" animal="Cat" breed="Tomcat" />
+      <Pet name="Pepper" animal="Bird" breed="Parrot" />
+      <Pet name="Bill" animal="Dog" breed="German Sherperd" />
+    </div>
   );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
