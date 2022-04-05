14aca1cf87dcdc542784fd249e41ca2693a53985
Emmanuel Ugwu <>
2022-04-05 18:21:01 +0100

    Changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
-
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adore Me!</h1>
-      <Pet name="Luna" animal="Cat" breed="Tomcat" />
+      <SearchParams />
+      {/* <Pet name="Luna" animal="Cat" breed="Tomcat" />
       <Pet name="Pepper" animal="Bird" breed="Parrot" />
-      <Pet name="Bill" animal="Dog" breed="German Sherperd" />
+      <Pet name="Bill" animal="Dog" breed="German Sherperd" /> */}
     </div>
   );
 };
---
+++ src/SearchParams.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+
+  function updateLocation(e){
+      setLocation(e.target.value)
+  }
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            onChange={updateLocation}
+            value={location.toUpperCase()}
+            placeholder="Location"
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
